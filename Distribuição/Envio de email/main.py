c9c5fa88396683f33c7443101c8c074d85a90993
Pedro Quintas <pedroquintas1213@gmail.com>
2024-10-07 09:26:13 -0300

    implementação de "Cc" e "Cco" direto do banco

--- Distribuição/Envio de email/main.py
+++ Distribuição/Envio de email/main.py
@@ -28,11 +28,12 @@ def enviar_emails():
         db_cursor = db_connection.cursor()
 
         # Puxar dados de configuração do SMTP
-        db_cursor.execute("SELECT smtp_host, smtp_port, smtp_username, smtp_password, url_thumbnail FROM companies LIMIT 1")
+        db_cursor.execute("""SELECT smtp_host, smtp_port, smtp_username, smtp_password, smtp_from_email, 
+                          smtp_from_name,smtp_reply_to, smtp_cc_emails,smtp_bcc_emails, url_thumbnail FROM companies LIMIT 1""")
         smtp_config = db_cursor.fetchone()
 
         if smtp_config:
-            smtp_host, smtp_port, smtp_user, smtp_password, logo = smtp_config
+            smtp_host, smtp_port, smtp_user, smtp_password,smtp_from_email,smtp_from_name,smtp_reply_to,smtp_cc_emails,smtp_bcc_emails,logo = smtp_config
         else:
             print("Configuração SMTP não encontrada.")
             exit()
@@ -41,27 +42,27 @@ def enviar_emails():
         exit()
 
     # Configuração do SMTP
-    smtp_config = (smtp_host, smtp_port, smtp_user, smtp_password, logo)
+    smtp_config = (smtp_host, smtp_port, smtp_user, smtp_password,smtp_from_email,smtp_from_name,smtp_reply_to,smtp_cc_emails,logo)
 
     for cliente, processos in clientes_data.items():
         localizador = str(uuid.uuid4()) 
         email_body = generate_email_body(cliente, processos, logo, localizador, data_do_dia)
-        email_receiver = "pedroquintas1213@gmail.com"
-        #email_receiver = processos[0]['emails']
-        bcc_receivers = ""  
+        email_receiver= processos[0]['emails']
+        bcc_receivers = smtp_bcc_emails 
+        cc_receiver = smtp_cc_emails
         subject = f"LIGCONTATO - DISTRIBUIÇÃO {data_do_dia.strftime('%d/%m/%y')} - {cliente}"
 
         # Envia o e-mail
-        send_email(smtp_config, email_body, email_receiver, bcc_receivers, subject)
+        send_email(smtp_config, email_body, email_receiver, bcc_receivers,cc_receiver, subject)
 
         try:
             for processo in processos:
                 processo_id = processo['ID_processo']  
                 db_cursor.execute("UPDATE processo SET status = 'S' WHERE ID_processo = %s", (processo_id,))
-                db_cursor.execute("""
-                    INSERT INTO envio_emails (ID_processo, numero_processo, cod_escritorio, localizador_processo, data_envio, localizador, email_envio, data_hora_envio)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                """, (processo_id, processo['numero_processo'], processo['cod_escritorio'], processo['localizador'], 
+                db_cursor.execute("""INSERT INTO envio_emails (ID_processo, numero_processo, 
+                                  cod_escritorio, localizador_processo, data_envio, localizador, email_envio, data_hora_envio)
+                                  VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
+                                    (processo_id, processo['numero_processo'], processo['cod_escritorio'], processo['localizador'], 
                       data_do_dia.strftime('%Y-%m-%d'), localizador, email_receiver, datetime.now()))
 
             db_connection.commit()  # Commit all changes
@@ -70,13 +71,13 @@ def enviar_emails():
         except mysql.connector.Error as err:
             print(f"Erro ao atualizar o status ou registrar o envio: {err}")
 
-# Agenda o envio para todos os dias às 16:00
-schedule.every().day.at("21:20").do(enviar_emails)
+#Agenda o envio para todos os dias às 16:00
+schedule.every().day.at("16:00").do(enviar_emails)
 
 if __name__ == "__main__":
+
     while True:
         schedule.run_pending()
-
             # A cada hora, recalcula os totais e exibe na tela
         clientes_data = fetch_processes_and_clients()
         total_escritorios = len(clientes_data)  
