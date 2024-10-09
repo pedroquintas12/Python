5108b4b742841f2c40a29aefd316e2fce8267eac
Pedro Quintas <pedroquintas1213@gmail.com>
2024-10-09 11:25:40 -0300

    configuração de pasta para Teste

--- faculdade.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df_original = pd.read_csv("C:\\Users\\pedro\\OneDrive\\Documentos\\GitHub\\Python\\dados.csv", sep=';',  encoding='ISO-8859-1')
-
-
-print(df_original.describe())
